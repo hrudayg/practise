4c94fb4da50309fecbf11426685210beeb370bbc
hruday.g <hruday.cse@gmail.com>
2017-07-28 09:53:54 +0530

    adding recursive method

--- GeeksForGeeks/src/com/practise/linkedlist/ReverseLinkedList.java
+++ GeeksForGeeks/src/com/practise/linkedlist/ReverseLinkedList.java
@@ -1,19 +1,28 @@
 package com.practise.linkedlist;
 
-import java.util.Arrays;
-
 public class ReverseLinkedList {
 	
 	public static void main(String[] args) {
 		
 		int input[] = {1, 2, 3, 4, 5};
-		System.out.println(Arrays.toString(input));
-		
 		LinkedList aList = LinkedListUtil.createLinkedList(input);
 		System.out.println(aList);
 		
 		reverse(aList);
 		System.out.println(aList);
+		
+		aList.head = reverseRecursive(aList.head);
+		System.out.println(aList);
+	}
+
+	private static LinkedListNode reverseRecursive(LinkedListNode head) {
+			if (head == null || head.next == null) {  
+			    return head;  
+			}
+			LinkedListNode remaining = reverseRecursive(head.next);
+			head.next.next = head; 
+			head.next = null;  
+			return remaining; 
 	}
 
 	private static void reverse(LinkedList aList) {
