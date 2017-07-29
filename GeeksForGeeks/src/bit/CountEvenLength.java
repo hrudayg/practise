c66dae972fbaaa9b2b6de5998ba8904f6714050f
hruday.g <hruday.cse@gmail.com>
2017-07-29 12:03:02 +0530

    more algos

---
+++ GeeksForGeeks/src/bit/CountEvenLength.java
@@ -0,0 +1,60 @@
+package bit;
+
+
+/*
+ * http://practice.geeksforgeeks.org/problems/count-even-length/0
+ *
+ */
+public class CountEvenLength {
+	
+	public static void main(String[] args) {
+		int n = 2;
+		countEvenLength(2 * n);
+	}
+
+	private static void countEvenLength(int length) {
+		StringBuffer sb = new StringBuffer();
+		while(length > 0){
+			sb.append("0");
+			length--;
+		}
+		
+		System.out.println(sb.toString());
+		//findAllCombinationsOfLength(sb.toString(), 0, sb.toString().length());
+	}
+
+	private static void findAllCombinations(String result, int length) {
+		
+		if(result.length() > length){
+			return;
+		}
+		
+		if(result.length() == length){
+			
+			String firstHalf = result.substring(0, length/2);
+			String secondHalf = result.substring(length / 2);
+			
+			int firstSum = 0;
+			int secondSum = 0;
+			
+			for(int index = 0; index < length/2; index++){
+				firstSum = firstSum + Integer.parseInt(""+firstHalf.charAt(index));
+			}
+			
+			for(int index = 0; index < length/2; index++){
+				secondSum = secondSum + Integer.parseInt(""+secondHalf.charAt(index));
+			}
+			
+			if(firstSum == secondSum){
+				System.out.println(result);
+			}
+		}
+		
+		String first = result+"0";
+		String second = result+"1";
+		
+		findAllCombinations(first, length);
+		findAllCombinations(second, length);
+	}
+
+}
