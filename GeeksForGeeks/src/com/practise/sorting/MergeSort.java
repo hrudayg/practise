42e6a25d7ecac851885d9381a0e0a8cdd8c13355
hruday.g <hruday.cse@gmail.com>
2017-08-26 20:51:42 +0530

    correcting comments

--- GeeksForGeeks/src/com/practise/sorting/MergeSort.java
+++ GeeksForGeeks/src/com/practise/sorting/MergeSort.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 /**
  * 
  * http://www.geeksforgeeks.org/merge-sort/
- * 
  * @author hruday.g
  *
  */
@@ -14,9 +13,9 @@ public class MergeSort {
 		int[] inputArr = { 12, 11, 13, 5, 7, 6 };
 		sort(inputArr);
 		System.out.println(Arrays.toString(inputArr));
-		//int[] testMergeArr = {11, 12, 13, 7, 5, 6};
-		//merge(testMergeArr, 0, 3, 5);
-		//System.out.println(Arrays.toString(testMergeArr));
+		// int[] testMergeArr = {11, 12, 13, 7, 5, 6};
+		// merge(testMergeArr, 0, 3, 5);
+		// System.out.println(Arrays.toString(testMergeArr));
 	}
 
 	private static void sort(int[] inputArr) {
@@ -24,22 +23,21 @@ public class MergeSort {
 	}
 
 	private static void mergeSort(int[] inputArr, int start, int end) {
-		if (start < end) {
-			int mid = (start + end) / 2;
-
-			mergeSort(inputArr, start, mid);
-			mergeSort(inputArr, mid + 1, end);
-
-			merge(inputArr, start, mid, end);
-
+		if (start >= end) {
+			return;
 		}
+		int mid = (start + end) / 2;
+
+		mergeSort(inputArr, start, mid);
+		mergeSort(inputArr, mid + 1, end);
+
+		merge(inputArr, start, mid, end);
 
 	}
 
 	private static void merge(int[] inputArr, int start, int mid, int end) {
-		int[] leftArr = new int[mid - start + 1] ;
+		int[] leftArr = new int[mid - start + 1];
 
-		// mid element will go to the righ sub array
 		int[] rightArr = new int[end - mid];
 
 		for (int i = 0; i < leftArr.length; i++) {
@@ -49,28 +47,28 @@ public class MergeSort {
 		for (int j = 0; j < rightArr.length; j++) {
 			rightArr[j] = inputArr[mid + j + 1];
 		}
-		
+
 		int leftIndex = 0;
 		int rightIndex = 0;
 		int inputArrIndex = start;
-		while(leftIndex < leftArr.length && rightIndex < rightArr.length){
-			if(leftArr[leftIndex] <= rightArr[rightIndex]){
+		while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+			if (leftArr[leftIndex] <= rightArr[rightIndex]) {
 				inputArr[inputArrIndex] = leftArr[leftIndex];
 				leftIndex++;
-			}else{
+			} else {
 				inputArr[inputArrIndex] = rightArr[rightIndex];
 				rightIndex++;
 			}
 			inputArrIndex++;
 		}
-		
-		while(leftIndex < leftArr.length){
+
+		while (leftIndex < leftArr.length) {
 			inputArr[inputArrIndex] = leftArr[leftIndex];
 			leftIndex++;
 			inputArrIndex++;
 		}
-		
-		while(rightIndex < rightArr.length){
+
+		while (rightIndex < rightArr.length) {
 			inputArr[inputArrIndex] = rightArr[rightIndex];
 			rightIndex++;
 			inputArrIndex++;
