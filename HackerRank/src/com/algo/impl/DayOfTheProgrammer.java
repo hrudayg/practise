648bb175dad79e0b526be9e4fea637f9a0bba215
hrudayg <hruday.gudepu@sap.com>
2017-10-01 09:33:37 +0530

    day of the programmer

---
+++ HackerRank/src/com/algo/impl/DayOfTheProgrammer.java
@@ -0,0 +1,31 @@
+package com.algo.impl;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+ * 
+ * @author hruday.g
+ *
+ */
+public class DayOfTheProgrammer {
+	static boolean solve(int year) {
+		boolean x = false;
+		if (year > 1918)
+			x = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+		else
+			x = (year % 4 == 0);
+		return x;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int year = in.nextInt();
+		if (year == 1918)
+			System.out.println("26.09.1918");
+		else if (solve(year))
+			System.out.println("12.09." + year);
+		else
+			System.out.println("13.09." + year);
+	}
+}
